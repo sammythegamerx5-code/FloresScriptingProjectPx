0aab73de9f66d6ab1310f8142de46a4a0206c21f
sammythegamerx5-code <sammythegamerx5@gmail.com>
2026-01-06 13:09:01 -0800

    Scripting Project
    Scope and Modifiers

---
+++ P6FloresScriptingProject/Assets/Scripts/AnotherClass.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnotherClass : MonoBehaviour
+{
+    public int apples = 67;
+    public int bananas;
+
+    private int stapler;
+    private int sellotape;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void FruitMachine(int a, int b)
+    {
+        int answer;
+        answer = a + b;
+        Debug.Log("Fruit total; " +  answer);
+    }
+    private void OfficeSort(int a, int b)
+    {
+        int answer;
+        answer = a + b;
+        Debug.Log("Office Supplies total: " + answer);
+    }
+}
